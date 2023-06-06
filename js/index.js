1aa4fe211636eb6d61a39e8ece954a29cf3cc36a
elliotgaramendi <elliotgaramendi@gmail.com>
2023-06-05 22:26:40 -0500

    feat: update root styles

---
+++ js/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log(document.querySelector('.header'));
+  console.log(document.querySelector('.nav'));
+
+  document.addEventListener('scroll', () => {
+    console.log('Arena')
+  });
+});
