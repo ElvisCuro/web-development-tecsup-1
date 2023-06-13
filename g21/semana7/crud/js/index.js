65e34d67ada2807ea0f03f3f2c8ef8a3e8e02c64
elliotgaramendi <elliotgaramendi@gmail.com>
2023-06-12 22:16:46 -0500

    feat: create g21/s7/1 js update

--- g21/semana7/crud/js/index.js
+++ g21/semana7/crud/js/index.js
@@ -4,24 +4,42 @@ window.addEventListener('DOMContentLoaded', () => {
   const pokemonTBody = document.getElementById('pokemonTBody');
   const pokemonForm = document.forms['pokemonForm'];
 
+  let pokemons = [
+    { name: 'Charmander', type: 'Fuego', image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png' },
+    { name: 'Squirtle', type: 'Agua', image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png' },
+    { name: 'Bulbasaur', type: 'Planta', image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png' }
+  ];
+
   pokemonForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const name = pokemonForm['name'].value;
     const type = pokemonForm['type'].value;
     const image = pokemonForm['image'].value;
 
-    pokemonTBody.innerHTML += `
-      <tr>
-        <td>n</td>
-        <td>${name}</td>
-        <td><span class="badge text-bg-danger">${type}</span></td>
-        <td>
-          <img src="${image}" alt="Charmander" width="32"
-            height="32" />
-        </td>
-      </tr>
-    `;
-
+    pokemons = [...pokemons, { name, type, image }];
+    readPokemons();
     pokemonForm.reset();
   });
+
+  const readPokemons = () => {
+    pokemonTBody.innerHTML = '';
+
+    pokemons.forEach((element, index) => {
+      const { name, type, image } = element;
+
+      pokemonTBody.innerHTML += `
+        <tr>
+          <td>${index + 1}</td>
+          <td>${name}</td>
+          <td><span class="badge text-bg-primary">${type}</span></td>
+          <td>
+            <img src="${image}" alt="Charmander" width="32"
+              height="32" />
+          </td>
+        </tr>
+      `;
+    });
+  };
+
+  readPokemons();
 });
