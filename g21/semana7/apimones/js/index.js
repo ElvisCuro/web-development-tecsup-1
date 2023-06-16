19a5c02969885c5a5214a4bc56d3419413beec39
elliotgaramendi <elliotgaramendi@gmail.com>
2023-06-15 22:00:18 -0500

    feat: update g21/s7/3 js header

--- g21/semana7/apimones/js/index.js
+++ g21/semana7/apimones/js/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
+import header from './modules/header.js';
 import loader from './modules/loader.js';
 
 const documentReady = () => {
   loader();
+  header();
 };
 
 document.addEventListener('DOMContentLoaded', documentReady);
