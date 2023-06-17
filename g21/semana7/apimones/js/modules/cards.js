382eb7031bbe83fedf92cde43690766fbd5d6ea8
elliotgaramendi <elliotgaramendi@gmail.com>
2023-06-16 22:56:14 -0500

    refactor: update g21/s7/3 fetch

--- g21/semana7/apimones/js/modules/cards.js
+++ g21/semana7/apimones/js/modules/cards.js
@@ -1,23 +1,20 @@
 import { renderPokemonCard } from "../../utils/util.js";
 
 const cards = () => {
-  const pokemonLocal = document.getElementById('pokemonLocal');
-  const pokemonRemote = document.getElementById('pokemonRemote');
+  const localPokemon = document.getElementById('localPokemon');
+  const remotePokemon = document.getElementById('remotePokemon');
 
-  const readPokemon = async () => {
-    const response = await fetch('https://elliotgaramendi.github.io/web-development-tecsup/g21/semana7/apimones/json/pokeapi-api-v2-pokemon-25.json');
-    const pokemon = await response.json();
-    renderPokemonCard(pokemonLocal, pokemon);
-  };
+  const readLocalPokemon = fetch('https://elliotgaramendi.github.io/web-development-tecsup/g21/semana7/apimones/json/pokeapi-api-v2-pokemon-25.json')
+    .then(response => response.json());
+  const readRemotePokemon = fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 151) + 1}`)
+    .then(response => response.json());
 
-  const readPokemon2 = async () => {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
-    const pokemon = await response.json();
-    renderPokemonCard(pokemonRemote, pokemon);
-  };
-
-  readPokemon();
-  readPokemon2();
+  Promise.all([readLocalPokemon, readRemotePokemon])
+    .then(data => {
+      const [dataLocalPokemon, dataRemotePokemon] = data;
+      renderPokemonCard(localPokemon, dataLocalPokemon);
+      renderPokemonCard(remotePokemon, dataRemotePokemon);
+    });
 };
 
 export default cards;
