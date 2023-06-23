e9adc9c435397300b9f10e5b7309fc82dc28e785
elliotgaramendi <elliotgaramendi@gmail.com>
2023-06-22 22:08:09 -0500

    feat: update g21/s8/1 hero card

--- g21/semana8/elliotgaramendi/src/components/Hero.jsx
+++ g21/semana8/elliotgaramendi/src/components/Hero.jsx
@@ -4,12 +4,14 @@ const Hero = () => {
       <img src="https://i.postimg.cc/x9MyhM3D/react-system.webp" alt="Fondo de portada" className="img img--background" />
       <div className="container">
         <div className="card" style={{ position: 'relative' }}>
-          <h1 className="card__subtitle">Elliot Garamendi</h1>
-          <h2 className="card__title">El mejor Frontend Developer y Docente</h2>
-          <p className="card__text">Desarrollador Web Frontend con React y Next.js. Docente de especialización en Desarrollo Web Frontend. Apasionado del autoaprendizaje y del código limpio.</p>
-          <div>
-            <button>Descargar CV</button>
-            <button>Contacto</button>
+          <div className="card__body">
+            <h1 className="card__subtitle">Elliot Garamendi</h1>
+            <h2 className="card__title">El mejor Frontend Developer y Docente</h2>
+            <p className="card__text">Desarrollador Web Frontend con React y Next.js. Docente de especialización en Desarrollo Web Frontend. Apasionado del autoaprendizaje y del código limpio.</p>
+            <div className="d-flex gap-2xs">
+              <button className="button button--outline-primary">Descargar CV</button>
+              <button className="button button--primary">Contacto</button>
+            </div>
           </div>
         </div>
       </div>
