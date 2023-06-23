5162fd6f4ead0d5c531e0ff6aa6600d9e3e3826b
elliotgaramendi <elliotgaramendi@gmail.com>
2023-06-22 21:17:06 -0500

    feat: update g21/s8/1 hero

--- g21/semana8/elliotgaramendi/src/components/Hero.jsx
+++ g21/semana8/elliotgaramendi/src/components/Hero.jsx
@@ -1,6 +1,19 @@
 const Hero = () => {
   return (
-    <h1>Elliot Garamendi</h1>
+    <article className="hero">
+      <img src="https://i.postimg.cc/x9MyhM3D/react-system.webp" alt="Fondo de portada" className="img img--background" />
+      <div className="container">
+        <div className="card" style={{ position: 'relative' }}>
+          <h1 className="card__subtitle">Elliot Garamendi</h1>
+          <h2 className="card__title">El mejor Frontend Developer y Docente</h2>
+          <p className="card__text">Desarrollador Web Frontend con React y Next.js. Docente de especialización en Desarrollo Web Frontend. Apasionado del autoaprendizaje y del código limpio.</p>
+          <div>
+            <button>Descargar CV</button>
+            <button>Contacto</button>
+          </div>
+        </div>
+      </div>
+    </article>
   );
 };
 
