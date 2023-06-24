6bfe186932eac9e6d28041958599ea7d1a90974a
elliotgaramendi <elliotgaramendi@gmail.com>
2023-06-24 11:08:52 -0500

    feat: update g21/s8/2 form input event

--- g21/semana8/patita-feliz/src/components/appointment/Form.jsx
+++ g21/semana8/patita-feliz/src/components/appointment/Form.jsx
@@ -1,26 +1,37 @@
+import { useState } from "react";
+
 const Form = () => {
+  const [appointmentForm, setAppointmentForm] = useState({});
+
+  const handleFormInput = (e) => {
+    setAppointmentForm({
+      ...appointmentForm,
+      [e.target.name]: e.target.value
+    });
+  };
+
   return (
     <section className="col-md-4">
       <h3>🐶 Registrar cita 🐶</h3>
       <form>
         <div className="form-floating mb-3">
-          <input type="text" className="form-control" id="petName" placeholder="Nombre de la mascota" />
+          <input type="text" name="petName" className="form-control" id="petName" placeholder="Nombre de la mascota" onInput={handleFormInput} />
           <label htmlFor="petName">Nombre de la mascota</label>
         </div>
         <div className="form-floating mb-3">
-          <input type="text" className="form-control" id="ownerName" placeholder="Nombre del propietario" />
+          <input type="text" name="ownerName" className="form-control" id="ownerName" placeholder="Nombre del propietario" onInput={handleFormInput} />
           <label htmlFor="ownerName">Nombre del propietario</label>
         </div>
         <div className="form-floating mb-3">
-          <input type="date" className="form-control" id="date" placeholder="Fecha" />
+          <input type="date" name="date" className="form-control" id="date" placeholder="Fecha" onInput={handleFormInput} />
           <label htmlFor="date">Fecha</label>
         </div>
         <div className="form-floating mb-3">
-          <input type="time" className="form-control" id="hour" placeholder="Hora" />
+          <input type="time" name="hour" className="form-control" id="hour" placeholder="Hora" onInput={handleFormInput} />
           <label htmlFor="hour">Hora</label>
         </div>
         <div className="form-floating mb-3">
-          <input type="text" className="form-control" id="symptoms" placeholder="Síntomas" />
+          <input type="text" name="symptoms" className="form-control" id="symptoms" placeholder="Síntomas" onInput={handleFormInput} />
           <label htmlFor="symptoms">Síntomas</label>
         </div>
         <button className="btn btn-primary w-100">Registrar</button>
