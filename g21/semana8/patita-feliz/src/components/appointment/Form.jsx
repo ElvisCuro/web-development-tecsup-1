2455dd0e97d0c3828dcac81c58e88bfde9e7962e
elliotgaramendi <elliotgaramendi@gmail.com>
2023-06-26 19:21:46 -0500

    feat: create g21/s8/2 appointments component

--- g21/semana8/patita-feliz/src/components/appointment/Form.jsx
+++ g21/semana8/patita-feliz/src/components/appointment/Form.jsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
 
 const Form = () => {
-  const [appointmentForm, setAppointmentForm] = useState({});
-  const [appointment, setAppointment] = useState({});
+  const [appointmentForm, setAppointmentForm] = useState({
+    petName: '',
+    ownerName: '',
+    date: '',
+    hour: '',
+    symptoms: ''
+  });
+  const [appointments, setAppointments] = useState([]);
+
+  const { petName, ownerName, date, hour, symptoms } = appointmentForm;
 
   const handleFormInput = (e) => {
     setAppointmentForm({
@@ -13,7 +21,17 @@ const Form = () => {
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    setAppointment(appointmentForm);
+    setAppointments([
+      ...appointments,
+      appointmentForm
+    ]);
+    setAppointmentForm({
+      petName: '',
+      ownerName: '',
+      date: '',
+      hour: '',
+      symptoms: ''
+    });
   };
 
   return (
@@ -22,23 +40,23 @@ const Form = () => {
         <h3>🐱 Registrar cita 🐱</h3>
         <form onSubmit={handleFormSubmit}>
           <div className="form-floating mb-3">
-            <input type="text" name="petName" className="form-control" id="petName" placeholder="Nombre de la mascota" onInput={handleFormInput} />
+            <input type="text" name="petName" className="form-control" id="petName" placeholder="Nombre de la mascota" value={petName} onInput={handleFormInput} />
             <label htmlFor="petName">Nombre de la mascota</label>
           </div>
           <div className="form-floating mb-3">
-            <input type="text" name="ownerName" className="form-control" id="ownerName" placeholder="Nombre del propietario" onInput={handleFormInput} />
+            <input type="text" name="ownerName" className="form-control" id="ownerName" placeholder="Nombre del propietario" value={ownerName} onInput={handleFormInput} />
             <label htmlFor="ownerName">Nombre del propietario</label>
           </div>
           <div className="form-floating mb-3">
-            <input type="date" name="date" className="form-control" id="date" placeholder="Fecha" onInput={handleFormInput} />
+            <input type="date" name="date" className="form-control" id="date" placeholder="Fecha" value={date} onInput={handleFormInput} />
             <label htmlFor="date">Fecha</label>
           </div>
           <div className="form-floating mb-3">
-            <input type="time" name="hour" className="form-control" id="hour" placeholder="Hora" onInput={handleFormInput} />
+            <input type="time" name="hour" className="form-control" id="hour" placeholder="Hora" value={hour} onInput={handleFormInput} />
             <label htmlFor="hour">Hora</label>
           </div>
           <div className="form-floating mb-3">
-            <input type="text" name="symptoms" className="form-control" id="symptoms" placeholder="Síntomas" onInput={handleFormInput} />
+            <input type="text" name="symptoms" className="form-control" id="symptoms" placeholder="Síntomas" value={symptoms} onInput={handleFormInput} />
             <label htmlFor="symptoms">Síntomas</label>
           </div>
           <button className="btn btn-primary w-100">Registrar</button>
@@ -47,16 +65,23 @@ const Form = () => {
       <section className="col-md-6">
         <h3>🐶 Citas 🐶</h3>
         <ul className="list-group">
-          {Object.keys(appointment).length && (
-            <li className="list-group-item list-group-item-action" aria-current="false">
-              <div className="d-flex w-100 justify-content-between">
-                <h5 className="mb-1">Mascota: {appointment.petName} 🐾</h5>
-                <small>📆 {appointment.date} 🕤 {appointment.hour}</small>
-              </div>
-              <p className="mb-1">Síntomas: {appointment.symptoms} 🩺</p>
-              <small>Dueño: {appointment.ownerName} 🤗</small>
-            </li>
-          )}
+          {appointments.map((element, index) => {
+            const { petName, date, hour, symptoms, ownerName } = element;
+            return (
+              <li
+                key={index}
+                className="list-group-item list-group-item-action"
+                aria-current="false"
+              >
+                <div className="d-flex w-100 justify-content-between">
+                  <h5 className="mb-1">Mascota: {petName} 🐾</h5>
+                  <small>📆 {date} 🕤 {hour}</small>
+                </div>
+                <p className="mb-1">Síntomas: {symptoms} 🩺</p>
+                <small>Dueño: {ownerName} 🤗</small>
+              </li>
+            );
+          })}
         </ul>
       </section>
     </>
