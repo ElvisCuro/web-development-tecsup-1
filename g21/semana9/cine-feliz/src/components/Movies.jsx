4db35c09e262abbbfbd8e5b117757b8145e0980d
elliotgaramendi <elliotgaramendi@gmail.com>
2023-06-26 22:10:51 -0500

    feat: create g21/s9/1 movies pagination

--- g21/semana9/cine-feliz/src/components/Movies.jsx
+++ g21/semana9/cine-feliz/src/components/Movies.jsx
@@ -3,10 +3,19 @@ import { useEffect, useState } from "react";
 const Movies = () => {
 
   const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const handlePreviusClick = () => {
+    setPage(page - 1);
+  }
+
+  const handleNextClick = () => {
+    setPage(page + 1);
+  }
 
   useEffect(() => {
     const fetchApi = async () => {
-      const url = 'https://api.themoviedb.org/3/discover/movie?page=1'
+      const url = `https://api.themoviedb.org/3/discover/movie?page=${page}`
       const options = {
         method: 'GET',
         headers: {
@@ -18,17 +27,21 @@ const Movies = () => {
       setMovies(data.results);
     };
     fetchApi();
-  }, []);
+  }, [page]);
 
   return (
     <section>
       <div>
         <h2>Películas</h2>
+        {page > 1 && <button onClick={handlePreviusClick}>Atrás</button>}
+        <button onClick={handleNextClick}>Siguiente</button>
         <div>
           {movies.map(element => {
+            const { id, title, poster_path } = element;
             return (
-              <div key={element.id}>
-                <h4>{element.title}</h4>
+              <div key={id}>
+                <img src={'https://image.tmdb.org/t/p/w300' + poster_path} alt={title} width={220} height={330} />
+                <h4>{title}</h4>
               </div>
             );
           })}
