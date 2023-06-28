d0804fb6fe54bb210ed6abe7b98588f4bb2ff5fd
elliotgaramendi <elliotgaramendi@gmail.com>
2023-06-27 22:46:04 -0500

    feat: create g21/s8/2 movie detail params

--- g21/semana9/cine-feliz/src/pages/MovieDetail.jsx
+++ g21/semana9/cine-feliz/src/pages/MovieDetail.jsx
@@ -1,6 +1,9 @@
+import { useParams } from "react-router-dom";
+
 const MovieDetail = () => {
+  const params = useParams();
   return (
-    <h1>Detalle de Película</h1>
+    <h1>Detalle de Película de id: {params.movieId}</h1>
   );
 };
 
