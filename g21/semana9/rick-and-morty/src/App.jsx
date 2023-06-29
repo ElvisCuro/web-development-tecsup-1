5b8936bfa5dc707b9dfbfa5db93403d1d6f51d06
elliotgaramendi <elliotgaramendi@gmail.com>
2023-06-28 23:02:44 -0500

    feat: create g21/s9/2 header

--- g21/semana9/rick-and-morty/src/App.jsx
+++ g21/semana9/rick-and-morty/src/App.jsx
@@ -1,10 +1,9 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Rick And Morty
-    </h1>
+    <Header />
   )
 }
 
