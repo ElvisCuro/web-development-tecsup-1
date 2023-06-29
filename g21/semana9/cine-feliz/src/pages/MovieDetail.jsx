fcf3f35615aab53db1dc2db05af133e3bd398a73
elliotgaramendi <elliotgaramendi@gmail.com>
2023-06-28 19:20:34 -0500

    feat: update g21/s8/2 movie detail fetchApi

--- g21/semana9/cine-feliz/src/pages/MovieDetail.jsx
+++ g21/semana9/cine-feliz/src/pages/MovieDetail.jsx
@@ -1,9 +1,31 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const MovieDetail = () => {
-  const params = useParams();
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const url = `https://api.themoviedb.org/3/movie/${movieId}?language=es-ES`
+      const options = {
+        method: 'GET',
+        headers: {
+          authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZjlmNzA1YjQ3ODRmNTQ3Mzc2ZjJjZDg4Mzg5MTY4NiIsInN1YiI6IjYyNTA1MjFlYTA1NWVmMDA2NTU3NTEzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jYHK5Rt55pBTHvGjrEGTjAXRVlGOdnDBpqBxhnhrCL4'
+        }
+      };
+      const response = await fetch(url, options);
+      const data = await response.json();
+      console.log(data);
+    };
+    fetchApi();
+  }, []);
+
   return (
-    <h1>Detalle de Película de id: {params.movieId}</h1>
+    <section className="section">
+      <div className="container">
+        <h1>Detalle de Película de id: {movieId}</h1>
+      </div>
+    </section>
   );
 };
 
