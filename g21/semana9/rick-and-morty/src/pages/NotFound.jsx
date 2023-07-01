898321abc8ec3f55d6f0d55d6889c6f6010a35ba
elliotgaramendi <elliotgaramendi@gmail.com>
2023-06-30 22:32:56 -0500

    feat: update g21/s9/2 header

--- g21/semana9/rick-and-morty/src/pages/NotFound.jsx
+++ g21/semana9/rick-and-morty/src/pages/NotFound.jsx
@@ -7,7 +7,7 @@ const NotFound = () => {
     <section className="not-found">
       <div className="not-found__container">
         <h1>404</h1>
-        <h2>{routeError.statusText}</h2>
+        <h2>{routeError.statusText || routeError.message}</h2>
         <Link to="/">Página de inicio</Link >
       </div>
     </section>
