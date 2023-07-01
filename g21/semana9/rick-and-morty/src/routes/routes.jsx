3d842f34ad52b5573054f11132bbaeaea979c2cc
elliotgaramendi <elliotgaramendi@gmail.com>
2023-06-30 20:19:28 -0500

    feat: create g21/s9/2 primarylayout

--- g21/semana9/rick-and-morty/src/routes/routes.jsx
+++ g21/semana9/rick-and-morty/src/routes/routes.jsx
@@ -1,21 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import PrimaryLayout from "../layouts/primaryLayout";
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />
-  },
-  {
-    path: '/personajes',
-    element: <h1>Personajes</h1>
-  },
-  {
-    path: '/ubicaciones',
-    element: <h1>Ubicaciones</h1>
-  },
-  {
-    path: '/episodios',
-    element: <h1>Episodios</h1>
+    element: <PrimaryLayout />,
+    children: [
+      {
+        index: true,
+        element: <App />
+      },
+      {
+        path: '/personajes',
+        element: <h1 style={{ padding: '5rem' }}>Personajes</h1>
+      },
+      {
+        path: '/ubicaciones',
+        element: <h1 style={{ padding: '5rem' }}>Ubicaciones</h1>
+      },
+      {
+        path: '/episodios',
+        element: <h1 style={{ padding: '5rem' }}>Episodios</h1>
+      },
+    ]
   },
 ]);
