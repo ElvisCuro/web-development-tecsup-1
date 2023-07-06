37bd668a50208a98920f7242d43386a36df0cf75
elliotgaramendi <elliotgaramendi@gmail.com>
2023-07-05 19:14:48 -0500

    feat: create g21/s10/1 products mocks

--- g21/semana10/tiendita-feliz/src/App.jsx
+++ g21/semana10/tiendita-feliz/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import { products } from './mocks/products.json';
 
 function App() {
+  console.log(products);
+
   return (
     <section className='py-12'>
       <div className="container p-4 mx-auto">
-        <h1 className='text-sky-700 text-3xl text-center'>🛒 Tiendita Feliz 🛒</h1>
+        <h1 className='text-sky-600 text-3xl font-black text-center'>🛒 Tiendita Feliz 🛒</h1>
       </div>
     </section>
   )
