49d1e53f3829a7f0208fd3d83149de2a15490429
elliotgaramendi <elliotgaramendi@gmail.com>
2023-07-05 21:58:04 -0500

    feat: create g21/s10/1 Header

--- g21/semana10/tiendita-feliz/src/App.jsx
+++ g21/semana10/tiendita-feliz/src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css'
+import Header from './components/Header';
 import Products from './components/Products';
 import { products } from './mocks/products.json';
 
 function App() {
   return (
-    <section className='py-12'>
-      <div className="container p-4 mx-auto flex flex-col gap-8">
-        <h1 className='text-sky-600 text-3xl font-black text-center'>🛒 Tiendita Feliz 🛒</h1>
-        <Products data={products} />
-      </div>
-    </section>
+    <>
+      <Header />
+      <section className='py-12'>
+        <div className="container px-4 mx-auto flex flex-col gap-8">
+          <Products data={products} />
+        </div>
+      </section>
+    </>
   )
 }
 
---
+++ g21/semana10/tiendita-feliz/src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="py-4">
+      <div className="container px-4 mx-auto">
+        <h1 className='text-sky-600 text-3xl font-black text-center'>🛒 Tiendita Feliz 🛒</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
