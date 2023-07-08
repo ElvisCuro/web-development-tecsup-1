c0f3519f0ce47165e57d66eabbf9e4dc7b6045fb
elliotgaramendi <elliotgaramendi@gmail.com>
2023-07-08 12:52:08 -0500

    feat: create g21/s10/3 counterThunk

--- g21/semana10/tiendita-feliz-redux/src/redux/store.jsx
+++ g21/semana10/tiendita-feliz-redux/src/redux/store.jsx
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./slice/counterSlice";
+import counterReducer from "./slices/counterSlice";
 
 export const store = configureStore({
   reducer: {
